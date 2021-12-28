42226884a7ac6562fff7d9b15417a35cf4173c34
Maciej <hoffix0123@gmail.com>
2021-12-28 22:20:39 +0100

    add: recovery component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import Dashboard from "./components/Dashboard";
+import Recover from "./components/Recover";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
+        <Route path="/" exact={true} element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/recover" element={<Recover />} />
       </Routes>
     </div>
   );
