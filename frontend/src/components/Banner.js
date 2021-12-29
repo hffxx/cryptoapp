ac340540a279748f20f399bed2d9787faaafe759
Maciej <hoffix0123@gmail.com>
2021-12-29 13:21:13 +0100

    add: api.js

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 const styles = {
   bannerContent: {
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-around",
-    height: "100vh",
+  },
+  bannerTitle: {
+    color: "#ffd434",
+    textShadow: "2px 2px black",
   },
 };
 function Banner() {
-  return <Container sx={styles.bannerContent}>Carousel</Container>;
+  return (
+    <Container sx={styles.bannerContent}>
+      <Typography variant="h2" sx={styles.bannerTitle}>
+        Price tracker
+      </Typography>
+    </Container>
+  );
 }
 
 export default Banner;
