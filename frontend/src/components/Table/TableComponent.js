517f1e1c3f2f625411e4c85592b32d4d35b63d67
Maciej <hoffix0123@gmail.com>
2022-01-02 01:06:24 +0100

    refactor: table component

--- frontend/src/components/Table/TableComponent.js
+++ frontend/src/components/Table/TableComponent.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Grid, Collapse, IconButton } from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 const percentColor = (coin) =>
@@ -14,12 +16,93 @@ const percentColor = (coin) =>
 const dataMissing = (
   <Typography sx={{ color: "orange" }}>Data missing</Typography>
 );
+
+function Row({ coin }) {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <TableRow
+        key={coin.id}
+        sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+      >
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          <Grid
+            item
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              gap: "20px",
+            }}
+          >
+            <Box component="img" src={coin?.image} sx={{ width: "25px" }}></Box>
+            <Typography>{coin.name}</Typography>
+            <Typography sx={{ color: "gray" }}>
+              {coin.symbol.toUpperCase()}
+            </Typography>
+          </Grid>
+        </TableCell>
+        <TableCell>
+          {!!coin.current_price ? (
+            <Grid item>
+              <Typography>{`$${coin.current_price}`}</Typography>
+            </Grid>
+          ) : (
+            dataMissing
+          )}
+        </TableCell>
+        <TableCell>
+          {!!coin.price_change_percentage_24h ? (
+            <Grid item sx={{ display: "flex", color: percentColor(coin) }}>
+              {coin.price_change_percentage_24h > 0 ? (
+                <ArrowDropUpIcon />
+              ) : (
+                <ArrowDropDownIcon />
+              )}
+              <Typography>{`${coin.price_change_percentage_24h}%`}</Typography>
+            </Grid>
+          ) : (
+            dataMissing
+          )}
+        </TableCell>
+        <TableCell>
+          <Grid item>
+            <Typography>{`$${coin.market_cap}`}</Typography>
+          </Grid>
+        </TableCell>
+        <TableCell>
+          <Grid item>
+            <Typography>{`$${coin.total_volume}`}</Typography>
+          </Grid>
+        </TableCell>
+        <TableCell>
+          <Grid item>
+            <Typography>{`${
+              coin.circulating_supply
+            } ${coin.symbol.toUpperCase()}`}</Typography>
+          </Grid>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+}
+
 function TableComponent({ data }) {
   return (
     <TableContainer>
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
+            <TableCell />
             <TableCell>Name</TableCell>
             <TableCell>Price</TableCell>
             <TableCell>24h %</TableCell>
@@ -30,63 +113,7 @@ function TableComponent({ data }) {
         </TableHead>
         <TableBody>
           {data.map((coin) => (
-            <TableRow
-              key={coin.id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: "20px",
-                  }}
-                >
-                  <Box
-                    component="img"
-                    src={coin?.image}
-                    sx={{ width: "25px" }}
-                  ></Box>
-                  <Typography>{coin.name}</Typography>
-                  <Typography sx={{ color: "gray" }}>
-                    {coin.symbol.toUpperCase()}
-                  </Typography>
-                </Box>
-              </TableCell>
-              <TableCell>
-                {!!coin.current_price ? (
-                  <Typography>{`$${coin.current_price}`}</Typography>
-                ) : (
-                  dataMissing
-                )}
-              </TableCell>
-              <TableCell>
-                {!!coin.price_change_percentage_24h ? (
-                  <Box sx={{ display: "flex", color: percentColor(coin) }}>
-                    {coin.price_change_percentage_24h > 0 ? (
-                      <ArrowDropUpIcon />
-                    ) : (
-                      <ArrowDropDownIcon />
-                    )}
-                    <Typography>{`${coin.price_change_percentage_24h}%`}</Typography>
-                  </Box>
-                ) : (
-                  dataMissing
-                )}
-              </TableCell>
-              <TableCell>
-                <Typography>{`$${coin.market_cap}`}</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>{`$${coin.total_volume}`}</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>{`${
-                  coin.circulating_supply
-                } ${coin.symbol.toUpperCase()}`}</Typography>
-              </TableCell>
-            </TableRow>
+            <Row coin={coin} />
           ))}
         </TableBody>
       </Table>
