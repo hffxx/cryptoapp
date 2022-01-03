30ee66f5b539d70e99163e8f79e8b2aa7aefd586
Maciej <hoffix0123@gmail.com>
2022-01-03 21:31:58 +0100

    feat: table pagination

--- frontend/src/components/Table/TableComponent.js
+++ frontend/src/components/Table/TableComponent.js
@@ -12,6 +12,8 @@ import {
   Collapse,
   IconButton,
   Hidden,
+  TablePagination,
+  Paper,
 } from "@mui/material";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
@@ -41,7 +43,7 @@ function Row({ coin }) {
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </TableCell>
-        <Hidden lgDown>
+        <Hidden xlDown>
           <TableCell>
             <Typography>{coin.market_cap_rank}</Typography>
           </TableCell>
@@ -83,39 +85,46 @@ function Row({ coin }) {
             dataMissing
           )}
         </TableCell>
-        <TableCell>
-          <Grid item>
-            <Typography>{`$${coin.market_cap}`}</Typography>
-          </Grid>
-        </TableCell>
-        <TableCell>
-          <Grid item>
-            <Typography>{`$${coin.total_volume}`}</Typography>
-          </Grid>
-        </TableCell>
-        <TableCell>
-          <Box>
-            <Typography>{`${
-              coin.circulating_supply
-            } ${coin.symbol.toUpperCase()}`}</Typography>
-            {!!coin.max_supply && coin.circulating_supply !== coin.max_supply && (
-              <Tooltip
-                title={`Percentage: ${(
-                  (coin.circulating_supply / coin.max_supply) *
-                  100
-                ).toFixed(2)}%`}
-                arrow
-              >
-                <LinearProgress
-                  sx={{ width: "150px", height: "5px", borderRadius: "2px" }}
-                  variant="determinate"
-                  value={(coin.circulating_supply / coin.max_supply) * 100}
-                  color="inherit"
-                ></LinearProgress>
-              </Tooltip>
-            )}
-          </Box>
-        </TableCell>
+        <Hidden mdDown>
+          <TableCell>
+            <Grid item>
+              <Typography>{`$${coin.market_cap}`}</Typography>
+            </Grid>
+          </TableCell>
+          <TableCell>
+            <Grid item>
+              <Typography>{`$${coin.total_volume}`}</Typography>
+            </Grid>
+          </TableCell>
+          <TableCell>
+            <Box>
+              <Typography>{`${
+                coin.circulating_supply
+              } ${coin.symbol.toUpperCase()}`}</Typography>
+              {!!coin.max_supply &&
+                coin.circulating_supply !== coin.max_supply && (
+                  <Tooltip
+                    title={`Percentage: ${(
+                      (coin.circulating_supply / coin.max_supply) *
+                      100
+                    ).toFixed(2)}%`}
+                    arrow
+                  >
+                    <LinearProgress
+                      sx={{
+                        width: "150px",
+                        height: "5px",
+                        borderRadius: "2px",
+                      }}
+                      variant="determinate"
+                      value={(coin.circulating_supply / coin.max_supply) * 100}
+                      color="inherit"
+                    ></LinearProgress>
+                  </Tooltip>
+                )}
+            </Box>
+          </TableCell>
+        </Hidden>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
@@ -153,30 +162,83 @@ function Row({ coin }) {
 }
 
 function TableComponent({ data }) {
+  const rows = data;
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangeRowsPerPage = (e) => {
+    setRowsPerPage(Number(e.target.value));
+    setPage(0);
+  };
+  const handleChangePage = (e, newPage) => {
+    setPage(newPage);
+  };
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  const props = {
+    page,
+    setPage,
+    rowsPerPage,
+    handleChangeRowsPerPage,
+    emptyRows,
+    rows,
+    handleChangePage,
+  };
   return (
     <TableContainer sx={{ overflowX: "auto" }}>
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell />
-            <Hidden lgDown>
+            <Hidden xlDown>
               <TableCell>#</TableCell>
             </Hidden>
             <TableCell>Name</TableCell>
             <TableCell>Price</TableCell>
             <TableCell>24h %</TableCell>
-            <TableCell>Market Cap</TableCell>
-            <TableCell>Volume</TableCell>
-            <TableCell>Circulating Supply</TableCell>
+            <Hidden mdDown>
+              <TableCell>Market Cap</TableCell>
+              <TableCell>Volume</TableCell>
+              <TableCell>Circulating Supply</TableCell>
+            </Hidden>
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((coin) => (
-            <Row coin={coin} key={coin.id} />
-          ))}
+          {data
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((coin) => (
+              <Row coin={coin} key={coin.id} />
+            ))}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 75 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
         </TableBody>
       </Table>
+      <TablePaginationComponent {...props} />
     </TableContainer>
+  );
+}
+
+function TablePaginationComponent({
+  rows,
+  rowsPerPage,
+  page,
+  handleChangePage,
+  handleChangeRowsPerPage,
+}) {
+  return (
+    <TablePagination
+      rowsPerPageOptions={[5, 10, 25]}
+      count={rows.length}
+      component={Paper}
+      rowsPerPage={rowsPerPage}
+      page={page}
+      onPageChange={handleChangePage}
+      onRowsPerPageChange={handleChangeRowsPerPage}
+    ></TablePagination>
   );
 }
 
