a34816e744e124f4f36df7c1df8ce85c622b4aed
Maciej <hoffix0123@gmail.com>
2022-01-09 20:54:16 +0100

    refactor: app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,28 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Dashboard from "./components/Dashboard";
 import Recover from "./components/Recover";
-import Sidebar from "./components/Sidebar";
-import { Hidden, Box } from "@mui/material";
+import { Box } from "@mui/material";
 import { AuthProvider } from "./components/contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Box sx={{ display: "flex" }}>
-        <AuthProvider>
+      <AuthProvider>
+        <Navbar />
+        <Box sx={{ display: "flex" }}>
           <Routes>
             <Route path="/" exact={true} element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/recover" element={<Recover />} />
           </Routes>
-        </AuthProvider>
-      </Box>
+        </Box>
+      </AuthProvider>
     </div>
   );
 }
