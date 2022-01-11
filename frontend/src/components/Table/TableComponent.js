5e7d57628888aaeaf0ba9e76b5cd4d77bade5f0f
Maciej <hoffix0123@gmail.com>
2022-01-11 20:39:24 +0100

    feat: error and success message to signup component

--- frontend/src/components/Table/TableComponent.js
+++ frontend/src/components/Table/TableComponent.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
@@ -14,6 +13,7 @@ import {
   Hidden,
   TablePagination,
 } from "@mui/material";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
