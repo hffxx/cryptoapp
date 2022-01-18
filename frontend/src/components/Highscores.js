308279873cc7c186af3fcb674f0a75561aefb9dc
Maciej <hoffix0123@gmail.com>
2022-01-18 21:33:27 +0100

    fix: bug with login/signup while logged in

--- frontend/src/components/Highscores.js
+++ frontend/src/components/Highscores.js
@@ -4,16 +4,17 @@ import { useAuth } from "./contexts/AuthContext";
 import { Typography } from "@mui/material";
 function Highscores() {
   const { userList } = useAuth();
-  console.log(userList);
-  const highscores = userList.sort((a, b) => {
-    return b.balance - a.balance;
-  });
+  const highscores =
+    userList &&
+    userList.sort((a, b) => {
+      return b.balance - a.balance;
+    });
   return (
     <DashboardPage>
       <Typography variant="h2" mb={5}>
         Highscores! (Testing)
       </Typography>
-      {userList.map((user) => {
+      {highscores.map((user) => {
         return (
           <div key={user.id}>{`Player ${user.nick} has $${user.balance}!`}</div>
         );
