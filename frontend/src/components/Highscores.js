c045a82fdc5c62e9292c7c6ca163735a18452846
Maciej <hoffix0123@gmail.com>
2022-01-18 21:59:35 +0100

    fix: bug with highscores

--- frontend/src/components/Highscores.js
+++ frontend/src/components/Highscores.js
@@ -4,17 +4,22 @@ import { useAuth } from "./contexts/AuthContext";
 import { Typography } from "@mui/material";
 function Highscores() {
   const { userList } = useAuth();
-  const highscores =
-    userList &&
-    userList.sort((a, b) => {
-      return b.balance - a.balance;
-    });
+  const highscores = () => {
+    let highscores = [];
+    if (userList) {
+      highscores = userList.sort((a, b) => {
+        return b.balance - a.balance;
+      });
+    }
+    return highscores;
+  };
+
   return (
     <DashboardPage>
       <Typography variant="h2" mb={5}>
         Highscores! (Testing)
       </Typography>
-      {highscores.map((user) => {
+      {highscores().map((user) => {
         return (
           <div key={user.id}>{`Player ${user.nick} has $${user.balance}!`}</div>
         );
